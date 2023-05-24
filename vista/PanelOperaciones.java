646b41802708f2eeea3b312eb739130b42203532
jjlizcano <jjlizcano6@gmail.com>
2023-05-24 18:32:24 -0500

    botones agregados

--- vista/PanelOperaciones.java
+++ vista/PanelOperaciones.java
@@ -24,6 +24,20 @@ public class PanelOperaciones extends JPanel
     {
         this.setLayout(null);
         this.setBackground(new Color(31,31,31));
+
+        btRegistrar = new JButton("Registrar");
+        btRegistrar.setBounds(100,40,120,20);
+        btRegistrar.setForeground(Color.WHITE);
+        btRegistrar.setBackground(new Color(59,59,59));
+        this.add(btRegistrar);
+        btRegistrar.setActionCommand("registrar");
+
+        btInformacion = new JButton("Información");
+        btInformacion.setBounds(300,40,120,20);
+        btInformacion.setForeground(Color.WHITE);
+        btInformacion.setBackground(new Color(59,59,59));
+        this.add(btInformacion);
+        btInformacion.setActionCommand("informacion");
     }
 
     public void agregarOyentesBotones(ActionListener pAL)
